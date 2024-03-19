3b7399bb55140de7358df79f52ac0b45f0b505be
Daniel Almopnte <d.almonte562003@gmail.com>
2024-03-19 12:07:31 +0100

    Test items & Items script start

--- Assets/Scripts/DanielScripts/Items.cs
+++ Assets/Scripts/DanielScripts/Items.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Items : MonoBehaviour
 {
+    public int affect;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,18 @@ public class Items : MonoBehaviour
     void Update()
     {
         
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Player")
+        {
+            return;
+        }
+        else if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log(collision.gameObject.name);
+            Debug.Log(this.gameObject.name + ": " + affect);
+
+        }
     }
 }
